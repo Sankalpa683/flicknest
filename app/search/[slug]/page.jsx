f2e3974e7e219b4844dc11fafa31592316da51a6
Sankalpa683 <sank.dahal@gmail.com>
2024-10-20 15:25:02 +0545

    improved movies section user expereince because of loading spinner bar

--- app/search/[slug]/page.jsx
+++ app/search/[slug]/page.jsx
@@ -111,37 +111,39 @@ const search = ({ params }) => {
                             Search Results
                         </h2>
                         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-8">
-                            {movies.map((movie, index) => (
-                                <a
-                                    key={index}
-                                    href="javascript:;"
-                                    className="mx-auto sm:mr-0 group cursor-pointer lg:mx-auto bg-white transition-all duration-500"
-                                >
-                                    <div className="">
-                                        <img
-                                            src={movie.img}
-                                            alt={`${movie.title} image`}
-                                            className="w-full aspect-square rounded-2xl object-cover"
-                                        />
-                                    </div>
-                                    <div className="mt-5">
-                                        <div className="flex items-center justify-between">
-                                            <h6 className="font-semibold text-xl leading-8 text-black transition-all duration-500 group-hover:text-indigo-600">
-                                                {movie.title}
-                                            </h6>
+                            {movies.map((movie, index) => {
+                                const slug = generateSlug(movie.title, movie.id);
+                                return (
+                                    <Link key={index} href={`/movies/${slug}`} onClick={handleLinkClick} className="mx-auto sm:mr-0 group cursor-pointer lg:mx-auto bg-white transition-all duration-500">
+                                        <div className="">
+                                            <img
+                                                src={movie.img}
+                                                alt={`${movie.title} image`}
+                                                className="w-full aspect-square rounded-2xl object-cover"
+                                            />
                                         </div>
-                                        <p className="font-normal text-sm leading-6 text-gray-500">
-                                            {movie.description}
-                                        </p>
-                                        <p className="font-normal text-sm leading-6 text-gray-500">
-                                            {movie.release_year}
-                                        </p>
-                                    </div>
-                                    <div className='hidden gap-1 flex-wrap justify-center lg:justify-start items-center lg:items-start mt-3'>
-                                        <Button type="primary"><PlayCircleOutlined /> Watch Now</Button>
-                                    </div>
-                                </a>
-                            ))}
+                                        <div className="mt-5">
+                                            <div className="flex items-center justify-between">
+                                                <h6 className="font-semibold text-xl leading-8 text-black transition-all duration-500 group-hover:text-indigo-600">
+                                                    {movie.title}
+                                                </h6>
+                                            </div>
+                                            <p className="font-normal text-sm leading-6 text-gray-500">
+                                                {movie.description}
+                                            </p>
+                                            <p className="font-normal text-sm leading-6 text-gray-500">
+                                                {movie.release_year}
+                                            </p>
+                                        </div>
+                                        <div className='hidden gap-1 flex-wrap justify-center lg:justify-start items-center lg:items-start mt-3'>
+
+                                            <Button type="primary"><PlayCircleOutlined /> Watch Now</Button>
+
+                                        </div>
+
+                                    </Link>
+                                );
+                            })}
                         </div>
                     </div>
                 </div>
