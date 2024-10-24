93a263edda92772dafaa0c559ea97d0d21db3797
Sankalpa Dahal <62802864+Sankalpa683@users.noreply.github.com>
2024-10-24 12:07:31 +0545

    Update siteConfig.js

--- app/config/siteConfig.js
+++ app/config/siteConfig.js
@@ -1,8 +1,8 @@
 // siteConfig.js
 export const siteConfig = {
-    name: 'BollyCinemaHub',
-    url: 'https://bollycinemahub.in',
-    description: "Welcome to BollyCinemaHub, your go-to platform to watch Bollywood movies online for free! We offer a vast collection of films from blockbusters to timeless classics, all at no cost and no registration required. Enjoy a cinematic experience like never before!",
+    name: 'BollyCinema Hub',
+    url: 'https://www.bollycinemahub.in/',
+    description: "Welcome to BollyCinema Hub, your go-to platform to watch Bollywood movies online for free! We offer a vast collection of films from blockbusters to timeless classics, all at no cost and no registration required. Enjoy a cinematic experience like never before!",
     twitterHandle: '@bollycinemahub',
     supportPhoneNumber: '+1-800-555-5555',
   };
