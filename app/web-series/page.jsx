26b281cd250de698712f55e2135306bb861d1c84
Sankalpa Dahal <62802864+Sankalpa683@users.noreply.github.com>
2024-10-29 20:02:46 +0545

    Update page.jsx

--- app/web-series/page.jsx
+++ app/web-series/page.jsx
@@ -9,7 +9,7 @@ const page = () => {
     return (
         <>
             <Navbar />
-            <div className="flex flex-col items-center justify-center w-full mx-auto p-4 bg-white">
+            <div className="flex flex-col items-center justify-center w-full mx-auto p-4 py-8 bg-white">
                 <div className="text-center">
                     <h1 className="text-[90px] font-extrabold text-indigo-600 mb-2">Coming Soon</h1>
                     <p className="text-2xl font-semibold text-gray-800">Web Series Coming Soon!</p><br />
